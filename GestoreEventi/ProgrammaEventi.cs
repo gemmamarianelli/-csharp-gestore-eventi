032d959e7959fa9e89af8302a06b7be9601dac41
Gemma Marianelli <115584815+gemmamarianelli@users.noreply.github.com>
2022-12-13 17:34:25 +0100

    inizio milestonen 3

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class ProgrammaEventi
+    {
+        private string Titolo;
+        private List<Evento> eventi;
+
+
+        public ProgrammaEventi(string titolo, List<Evento> eventi)
+        {
+            this.Titolo = titolo;
+            this.eventi = eventi;
+        }
+
+
+    }
+}
